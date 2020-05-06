e644ac1718181caf53904234f8b71e3adfa07e3f
makemanback <makemanback@gmail.com>
2020-05-06 15:00:32 +0300

    сборка_финал

--- gulpfile.js
+++ gulpfile.js
@@ -93,21 +93,6 @@ gulp.task("copy", function() {
     .pipe(gulp.dest("build"));
 });
 
-gulp.task("server", function () {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-
-  gulp.watch("source/sass/**/*.scss", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
-});
-
-gulp.task("start", gulp.series("css", "server"));
-
 // запуск build
 gulp.task("build", gulp.series(
   "clean",
@@ -116,3 +101,25 @@ gulp.task("build", gulp.series(
   "css",
   "html"
 ));
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.scss", gulp.series("css"));
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+// обновление сервера
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("start", gulp.series("build", "server"));
